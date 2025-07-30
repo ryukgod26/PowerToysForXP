b0781f748bbf87f9811bde64dd46d304a65d27c6
raviwhitehat <126102004+raviwhitehat@users.noreply.github.com>
2025-07-30 22:43:53 +0530

    Created a basic Window app with which take input and show output in dialog box.

--- PowerToysForXP/Form1.cs
+++ PowerToysForXP/Form1.cs
@@ -14,6 +14,79 @@ namespace PowerToysForXP
         public Form1()
         {
             InitializeComponent();
+            textBox4.Text = progressBar1.Value.ToString();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked!");
+            if (textBox1.Text.Length > 0 && textBox2.Text.Length > 0)
+            {
+                MessageBox.Show($"Hello {textBox1.Text} {textBox2.Text}");
+                MessageBox.Show($"You are {textBox3.Text} Years Old.");
+
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show("Thanks For Accepting Termas and Conditions.");
+                    checkBox1.Dispose();
+                    progressBar1.Value = 100;
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Please Accept Terms and Conditions First.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Enter your name.");
+            }
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
